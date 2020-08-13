2f607b0b00f27420ded60b774c4fee623fce023f
Roger Pawlowski <rppawlo@sandia.gov>
2020-08-13 07:58:26 -0600

    LOCA: adding tpetra householder bordered solver

---
+++ packages/nox/src-loca/src-tpetra/NOX_TpetraTypedefs.hpp
@@ -0,0 +1,35 @@
+#ifndef NOX_TPETRA_TYPEDEFS_HPP
+#define NOX_TPETRA_TYPEDEFS_HPP
+
+#include "Tpetra_ConfigDefs.hpp"
+#include "Tpetra_Map_fwd.hpp"
+#include "Tpetra_MultiVector_fwd.hpp"
+#include "Tpetra_Vector_fwd.hpp"
+#include "Tpetra_Export_fwd.hpp"
+#include "Tpetra_Import_fwd.hpp"
+#include "Tpetra_CrsGraph_fwd.hpp"
+#include "Tpetra_CrsMatrix_fwd.hpp"
+#include "Tpetra_Operator_fwd.hpp"
+
+namespace NOX {
+  
+  using Scalar = double;
+  using LocalOrdinal = int;
+  using GlobalOrdinal = Tpetra::Map<>::global_ordinal_type;
+  using GlobalSizeType = Tpetra::global_size_t;
+  using DeviceSpace = Kokkos::DefaultExecutionSpace;
+  using NodeType = Kokkos::Compat::KokkosDeviceWrapperNode<DeviceSpace>;
+  
+  using TMap = Tpetra::Map<LocalOrdinal, GlobalOrdinal, NodeType>;
+  using ConstTMap = const Tpetra::Map<LocalOrdinal, GlobalOrdinal, NodeType>;
+  using TImport = Tpetra::Import<LocalOrdinal,GlobalOrdinal,NodeType>;
+  using TExport = Tpetra::Export<LocalOrdinal,GlobalOrdinal,NodeType>;
+  using TVector = Tpetra::Vector<Scalar,LocalOrdinal,GlobalOrdinal,NodeType>;
+  using TMultiVector = Tpetra::MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,NodeType>;
+  using TCrsGraph = Tpetra::CrsGraph< LocalOrdinal, GlobalOrdinal, NodeType>;
+  using TCrsMatrix = Tpetra::CrsMatrix< Scalar, LocalOrdinal, GlobalOrdinal, NodeType>;
+  using TOperator =  Tpetra::Operator<Scalar, LocalOrdinal, GlobalOrdinal, NodeType>;
+
+}
+
+#endif
